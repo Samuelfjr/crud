c149a39f39fa619663be7a3d001155faa4a5de15
Samuelfjr <devsamuelfjr@gmail.com>
2024-06-07 06:41:32 -0300

    ADS:01609372 rest-cli

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,40 +1,26 @@
-let people = [
-  { id: 1, name: "João Silva", email: "joao@example.com" },
-  { id: 2, name: "Maria Souza", email: "maria@example.com" },
-];
+/**
+ * Nome do arquivo: users.js
+ * Data de criação:
+ * Autor: mateus micael p.rabelo
+ * Matrícula: 01609372
+ *
+ * Descrição:
+ * Este arquivo é responsável pelo CRUD da collection de clients
+ * fazendo a manipulação dos dados conformme o tipo de requisição realizada.
+ *
+ * Este script é parte o curso de ADS.
+ */
 
 export default function handler(req, res) {
-  if (req.method === "GET") {
-    res.status(200).json(people);
-  } else if (req.method === "POST") {
-    const { name, email } = req.body;
-    const newPerson = { id: people.length + 1, name, email };
-    people.push(newPerson);
-    res.status(201).json(newPerson);
-  } else if (req.method === "PUT") {
-    const { id } = req.query;
-    const { name, email } = req.body;
-    const personIndex = people.findIndex(
-      (person) => person.id === parseInt(id)
-    );
-    if (personIndex !== -1) {
-      people[personIndex] = { id: parseInt(id), name, email };
-      res.status(200).json(people[personIndex]);
-    } else {
-      res.status(404).json({ message: "Pessoa não encontrada" });
-    }
-  } else if (req.method === "DELETE") {
-    const { id } = req.query;
-    const personIndex = people.findIndex(
-      (person) => person.id === parseInt(id)
-    );
-    if (personIndex !== -1) {
-      people.splice(personIndex, 1);
-      res.status(200).json({ message: "Pessoa excluída com sucesso" });
-    } else {
-      res.status(404).json({ message: "Pessoa não encontrada" });
-    }
-  } else {
-    res.status(405).json({ message: "Método não permitido" });
+  if (req.method == "GET") {
+    res.status(200).json("Local onde será retornado os dados dos clientes");
+  } else if (req.method == "POST") {
+    res.status(200).json("Local onde serão adicionadas novos clientes");
+  }
+
+  if (req.method == "PUT") {
+    res.status(200).json("Local onde os clientes terão seus dados alterados");
+  } else if (req.method == "DELETE") {
+    res.status(200).json("Local onde os clientes serão apagados");
   }
 }
