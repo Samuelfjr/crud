b3a39a6bf8c9bea08a1bfc2ae5103567624a0b03
Samuelfjr <devsamuelfjr@gmail.com>
2024-06-07 06:22:54 -0300

    refactor: firebase

--- config/firebaseconfig.js
+++ config/firebaseconfig.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC267d9EsIf-Jj3q8NOe8-AQX2P25DrO70",
-  authDomain: "perfectcrud.firebaseapp.com",
-  projectId: "perfectcrud",
-  storageBucket: "perfectcrud.appspot.com",
-  messagingSenderId: "178563042062",
-  appId: "1:178563042062:web:d4d9485f7117de982be55f",
-  measurementId: "G-REXWX37K4E"
+  apiKey: "AIzaSyB_aynD3zIbDBdlr8NV-9DAli4UESWDLnw",
+  authDomain: "crudchico-77304.firebaseapp.com",
+  projectId: "crudchico-77304",
+  storageBucket: "crudchico-77304.appspot.com",
+  messagingSenderId: "991561904801",
+  appId: "1:991561904801:web:316779f25ba6cedc0af6e0",
+  measurementId: "G-CBNNCRNP1T",
 };
 
 // Initialize Firebase
--- pages/api/app.js
+++ pages/api/app.js
@@ -1,18 +1,16 @@
 import { useEffect } from "react";
 import firebase from "firebase/app";
-import "firebase/firestore"; 
+import "firebase/firestore";
 
-import firebaseConfig from "../config/firebaseconfig"; 
-
+// import firebaseConfig from "../config/firebaseconfig";
+import firebaseConfig from "../../config/firebaseconfig";
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
 function MyApp({ Component, pageProps }) {
-  
   useEffect(() => {
-    
     if (!firebase.apps.length) {
       firebase.initializeApp(firebaseConfig);
     }
