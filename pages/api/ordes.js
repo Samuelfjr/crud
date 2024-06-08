f8574914bfa0ca6d83c0a0ab75630ac013dac335
PyxisCH120 <mateusmicaelpeixoto@gmail.com>
2024-06-07 21:09:14 -0300

    ADS: 01609372 DB

--- pages/api/ordes.js
+++ pages/api/ordes.js
@@ -11,14 +11,122 @@
  * Este script é parte o curso de ADS.
  */
 
-export default function handler(req, res) {
+import db from "./firebase.js";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
+
+export default async function handler(req, res) {
   if (req.method == "GET") {
-    res.status(200).json("Local onde será retornado os dados das ordens");
+    const getData = async () => {
+      let data = [];
+
+      const ordens = await getDocs(collection(db, "ordensServico"));
+
+      ordens.forEach((doc) => {
+        let tempData = doc.data();
+        tempData["id"] = doc.id;
+        data.push(tempData);
+
+        //data[doc.id] = doc.data();
+      });
+      res.status(200).json(data);
+    };
+
+    getData();
   } else if (req.method == "POST") {
-    res.status(200).json("Local onde serão adicionadas novas ordens");
+    const insertData = async (client, date, describe, cost) => {
+      await addDoc(collection(db, "ordensServico"), {
+        status: "iniciado",
+        client: client,
+        date: date,
+        describe: describe,
+        cost: cost,
+      });
+      if (insertData) {
+        res.status(201).json("Usuário criado com sucesso");
+      } else {
+        res.status(500).json("Erro ao inserir o usuário no banco");
+      }
+    };
+
+    const update = async (id, status, client, date, describe, cost) => {
+      // Organiza a inserção dos dados
+      const idOrdem = id;
+      const valid_ordem = doc(db, "ordensServico", idOrdem);
+
+      // Atualiza os dados
+      await updateDoc(valid_ordem, {
+        status: status,
+        client: client,
+        date: date,
+        describe: describe,
+        cost: cost,
+      }).then(() => {
+        res.status(200).json("Dados alterados com sucesso");
+      });
+    };
+
+    const dataReceived = req.body;
+    if (dataReceived.id) {
+      console.log("Dados:", dataReceived);
+      update(
+        dataReceived.id,
+        dataReceived.status,
+        dataReceived.client,
+        dataReceived.date,
+        dataReceived.describe,
+        dataReceived.cost
+      );
+      res.json("Alterar");
+    } else {
+      insertData(
+        dataReceived.client,
+        dataReceived.date,
+        dataReceived.describe,
+        dataReceived.cost
+      );
+    }
   } else if (req.method == "PUT") {
-    res.status(200).json("Local onde as ordens terão seus dados alterados");
+    const getOrdem = async (data) => {
+      const id = data.id;
+      const datadb = await getDoc(doc(db, "ordensServico", id));
+      let tempData = datadb.data();
+      tempData["id"] = datadb.id;
+      res.status(302).json(tempData);
+    };
+    const dataReceived = req.query;
+    if (
+      (dataReceived.id,
+      dataReceived.status &&
+        dataReceived.client &&
+        dataReceived.date &&
+        dataReceived.describe &&
+        dataReceived.cost)
+    ) {
+      console.log("Dados recebidos: ", dataReceived);
+      res.json("Dados recebidos: ", dataReceived);
+    } else {
+      getOrdem(dataReceived);
+    }
   } else if (req.method == "DELETE") {
-    res.status(200).json("Local onde as ordens serão apagadas");
+    const deleto = async (id, table) => {
+      console.log(id);
+
+      const idClient = id;
+      const valid_usuario = doc(db, table, idClient);
+
+      await deleteDoc(valid_usuario).then(() => {
+        res.status(200).json("Ordem apagada");
+      });
+    };
+    const dataReceived = req.query;
+    deleto(dataReceived.id, "ordensServico");
   }
 }
